bab2bcb375366623b359ba1795b89185c744435a
hayasaka yudai <hyudai@hayasakanoMacBook-Pro.local>
2022-04-28 17:19:46 +0900

    fix fibonacci

--- go02/ex04/vendor/piscine/fibonacci.go
+++ go02/ex04/vendor/piscine/fibonacci.go
@@ -4,9 +4,11 @@ func Fibonacci(index int) int {
 	var ret int = 0
 
 	if index < 0 {
-		return (0)
+		return -1
 	} else if index == 1 {
 		return 1
+	} else if index == 0 {
+		return 0
 	}
 	ret = Fibonacci(index - 1) + Fibonacci(index - 2)
 	return ret
